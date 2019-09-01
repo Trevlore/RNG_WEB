49a92e31e5de94cf8de09a73d989a6f29e532532
Trevlore <tbrow189@mtroyal.ca>
2019-09-01 15:50:09 -0600

    Data loop now works on general data in the array.

--- rng.js
+++ rng.js
@@ -1,12 +1,5 @@
-
-
-
-document.addEventListener('DOMContentLoaded', function (){
-    
-    // var div = document.createElement("div");
-    // div.innerHTML = "generate this list";
-    // document.getElementById("CharacterElements").appendChild(div);
-
+document.addEventListener('DOMContentLoaded', function () {
+    createElementSelection()
 });
 
 function getRandomNumber(max) {
@@ -22,19 +15,39 @@ function generateCharacter() {
     var character = "";
     for (let index = 0; index < userInputs.length; index++) {
         if (userInputs[index].checked) {
-            character +=  randomIndex(data[userInputs[index].name]) + " ";
+            character += randomIndex(data[userInputs[index].name]) + " ";
         }
     }
-    updateCharactor(character);
+    updateCharacter(character);
 }
 
-function updateCharactor(character) {
+function updateCharacter(character) {
     var outputDiv = document.getElementById('CharacterDescription');
-    outputDiv.innerHTML = "Random Charactor Generated " + character;
+    outputDiv.innerHTML = "Random Character Generated " + character;
 }
 
 function getElementSelection() {
     return document.getElementsByTagName('input');
 }
 
+function createElementSelection() {
+    const keys = Object.keys(data)
+    for (let index = 0; index < keys.length; index++) {
+        var div  = document.createElement("div");
+        
+        var label = document.createElement("label");
+        label.setAttribute("class","checkbox");
+        label.innerText  = keys[index];
+               
+        var input = document.createElement("input");
+        input.setAttribute("type","checkbox");
+        input.setAttribute("name",keys[index]);
+        
+        
+        div.appendChild(label)
+        label.appendChild(input)
+        
+        document.getElementById("CharacterElements").appendChild(div);
+    }
+}
 
